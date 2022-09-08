cdf9717d5c3e9eed32ed341f4e5e8ac94112b6e7
Oluwadebo <ogunweoluwadebo@gmail.com>
2022-09-08 12:44:13 +0100

    file succesfuly created

---
+++ src/component/Signup.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 mx-auto">
+            <form action="">
+              <h2>Create Account</h2>
+              <input type="text" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
