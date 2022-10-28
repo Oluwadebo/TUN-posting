3339039fbaaf0bc42b8a6bd44019ba76aeafffcf
Oluwadebo <ogunweoluwadebo@gmail.com>
2022-10-28 10:48:53 +0100

    navbar done

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import {
 } from "react-router-dom";
 import NotFound from "./component/NotFound";
 import Nysc from "./component/Nysc";
+import Signup from "./component/Signup";
 
 function App() {
   return (
     <>
+      <Nysc />
       <Router>
         <Routes>
-          <Route path="/" element={<Nysc />} />
+          <Route path="/" element={<Signup />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
--- src/component/Nysc.js
+++ src/component/Nysc.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import NYSC from './assest/NYSC-LOGO.jpg'
 
 const Nysc = () => {
     return (
         <>
-        <div className="container-fliud">
-            <h2>NYSC</h2>
-        </div>
+            <div>
+                <div className="container bg-white">
+                    <div className="row">
+                        <div className="col col-md-4 ded" style={{ background: "#447502" }}></div>
+                        <div className="col col-md-4">
+                            <div className="py-md-4 py-2" style={{ background: "#447502" }}></div>
+                            <img src={NYSC} alt="" className="img-fluid rig" />
+                        </div>
+                        <div className="col col-md-4 let" style={{ background: "#447502" }}></div>
+                    </div>
+                </div>
+                <div className='container'>
+                    <center>
+                        <h1> <span className='bg-white px-4'>NYSC POSTING SYSTEM</span></h1>
+                    </center>
+                </div>
+
+            </div>
         </>
     )
 }
