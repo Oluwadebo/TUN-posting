37b25462041ce65e1876a704df7ce7d1f8331b92
Oluwadebo <ogunweoluwadebo@gmail.com>
2022-10-28 08:23:27 +0100

    nysc page created

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import Signup from "./component/Signup";
 import NotFound from "./component/NotFound";
-import Signin from "./component/Signin";
-import Dashboard from "./component/Dashboard";
+import Nysc from "./component/Nysc";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/Signin" element={<Signin />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Signup" element={<Navigate to="/" />} />
+          <Route path="/" element={<Nysc />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
