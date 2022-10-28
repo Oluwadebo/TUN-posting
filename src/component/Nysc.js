8c7316020cfc1135800ab549b1388e27aa229982
Oluwadebo <ogunweoluwadebo@gmail.com>
2022-10-28 08:37:17 +0100

    ready to start

--- src/component/Nysc.js
+++ src/component/Nysc.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Nysc = () => {
     return (
-        <></>
+        <>
+        <div className="container-fliud">
+            <h2>NYSC</h2>
+        </div>
+        </>
     )
 }
 
